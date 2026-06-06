02514679c569a22bf6959b347bd1bd46be5fdb6d
Feliberto Alcantara <mrprincevinsmoke50@gmail.com>
2026-06-06 12:36:39 -0400

    Creación de DTOs para el registro y respuesta de usuarios

---
+++ SanApi/Dtos/UsuarioRegistroDto.cs
@@ -0,0 +1,10 @@
+﻿namespace SanApi.Dtos
+{
+    public class UsuarioRegistroDto
+    {
+        public string NombreCompleto { get; set; } = string.Empty;
+        public string Correo { get; set; } = string.Empty;
+        public string Telefono { get; set; } = string.Empty;
+        public string Contrasena { get; set; } = string.Empty;
+    }
+}
---
+++ SanApi/Dtos/UsuarioRespuestaDto.cs
@@ -0,0 +1,10 @@
+﻿namespace SanApi.Dtos
+{
+    public class UsuarioRespuestaDto
+    {
+        public Guid Id { get; set; }
+        public string NombreCompleto { get; set; } = string.Empty;
+        public string Correo { get; set; } = string.Empty;
+        public string Telefono { get; set; } = string.Empty;
+    }
+}
